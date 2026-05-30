0ae3a48f187dbf7e8e5869918406b8df69aec05e
DemonKingSwarn <swarnaditya.7@proton.me>
2026-05-30 15:08:42 +0530

    build script cleanups and organization

--- nob.c
+++ nob.c
@@ -1,13 +1,21 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
+const char *cflags[] = {"-I./include", "./lib/libraylib.a", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lubsan"};
+
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
+
   nob_cmd_append(&cmd, "mkdir", "-p", "build");
   if (!nob_cmd_run(&cmd)) return 1;
   cmd.count = 0;
-  nob_cmd_append(&cmd, "clang", "main.c", "-I./include", "./lib/libraylib.a", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lubsan", "-o", "build/main", "-pedantic");
+
+  nob_cmd_append(&cmd, "clang", "main.c", "-o", "build/main", "-pedantic");
+  for (size_t i = 0; i < NOB_ARRAY_LEN(cflags); i++) {
+    nob_cmd_append(&cmd, cflags[i]);
+  }
+
   if (!nob_cmd_run(&cmd)) return 1;
   return 0;
 }
