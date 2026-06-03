b12ccfe9dfea643f4ba70fec87b88de3e641f05a
DemonKingSwarn <swarnaditya.7@proton.me>
2026-06-03 15:56:48 +0530

    chore: made the engine a lot like Unity

---
+++ include/purgatory.h
@@ -0,0 +1,5 @@
+void Awake();
+void Start();
+void Update(float delta);
+void LateUpdate(float delta);
+void FixedUpdate(float fixedDelta);
--- main.c
+++ main.c
@@ -1,23 +1,55 @@
 #include "raylib.h"
 #include "config.h"
+#include "purgatory.h"
 
 float timeScale = 1.0f;
+float fixedTimeStep = .0f;
+float accumulator = .0f;
 
 int main(void) {
-  SetTargetFPS(fps);
-  InitWindow(width, height, title);
+  Awake();
+
+  Start();
+
+
+  fixedTimeStep = 1.0f / 50.0f * timeScale;
+  accumulator = 0.0;
 
   while (!WindowShouldClose()) {
     float delta = GetFrameTime() * timeScale;
 
     ClearBackground(GRAY);
 
-    BeginDrawing();
-      DrawText("This is a text!", GetScreenWidth() / 2, GetScreenHeight() / 2, 50, RAYWHITE);
-    EndDrawing();
+    Update(delta);
+
+    accumulator += GetFrameTime();
+
+    while (accumulator >= fixedTimeStep) {
+      FixedUpdate(fixedTimeStep);
+      accumulator -= fixedTimeStep;
+    }
   }
 
   CloseWindow();
 
   return 0;
+}
+
+void Awake() {
+  SetTargetFPS(60);
+  InitWindow(width, height, title);
+}
+
+void Start() {
+
+}
+
+void Update(float delta) {
+  BeginDrawing();
+    DrawText("this is a text", GetScreenWidth() / 2, GetScreenHeight() / 2, 20, RAYWHITE);
+  EndDrawing();
+}
+
+void FixedUpdate(float fixedDelta) {
+
 }
